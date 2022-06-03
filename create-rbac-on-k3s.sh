a582e7cca2cd0ae16658b67f76d787923d829823
Pierre Baconnier <pierre.baconnier@outlook.fr>
2022-06-03 15:24:48 +0200

    feat: add mysql chart

--- create-rbac-on-k3s.sh
+++ create-rbac-on-k3s.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #https://stackoverflow.com/questions/67210000/rbac-role-based-access-control-on-k3s
 
-openssl genrsa -out pbackz.key 2048
-openssl req -new -key pbackz.key -out pbackz.csr -subj "/CN=pbackz/O=pbackz_grp"
+openssl genrsa -out toto.key 2048
+openssl req -new -key toto.key -out toto.csr -subj "/CN=toto/O=toto_grp"
 
 cat <<EOF | kubectl apply -f -
 apiVersion: certificates.k8s.io/v1
 kind: CertificateSigningRequest
 metadata:
-  name: pbackz
+  name: toto
 spec:
   groups:
   - system:authenticated
-  request: $(cat pbackz.csr | base64 | tr -d '\n')
+  request: $(cat toto.csr | base64 | tr -d '\n')
   signerName: kubernetes.io/kube-apiserver-client
   usages:
   - client auth
@@ -20,45 +20,72 @@ EOF
 
 kubectl get csr
 
-kubectl certificate approve pbackz
+kubectl certificate approve toto
 
-kubectl get csr pbackz -o jsonpath='{.status.certificate}'  | base64 -d > pbackz.crt
+kubectl get csr toto -o jsonpath='{.status.certificate}'  | base64 -d > toto.crt
 
 cat <<EOF | kubectl apply -f - 
 apiVersion: rbac.authorization.k8s.io/v1
 kind: Role
 metadata:
-  name: pbackz-role
+  name: toto-role
 rules:
 - apiGroups:
   - ""
   resources:
   - pods
+  - secrets
+  - services
   verbs:
   - get
   - list
+  - watch
+  - update
+EOF
+
+cat <<EOF | kubectl apply -f - 
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: toto-role-2
+rules:
+- apiGroups:
+  - ""
+  resources:
+  - pods
+  - secrets
+  - services
+  - deployments
+  - configmaps
+  verbs:
+  - get
+  - list
+  - create
+  - delete
+  - update
 EOF
 
 cat <<EOF | kubectl apply -f - 
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
-  name: pbackz-binding
+  name: toto-binding-2
+  namespace: default
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: Role
-  name: pbackz-role
+  name: toto-role-2
 subjects:
 - apiGroup: rbac.authorization.k8s.io
   kind: User
-  name: pbackz
+  name: toto
 EOF
 
-kubectl config set-credentials pbackz --client-key=pbackz.key --client-certificate=pbackz.crt --embed-certs=true
+kubectl config set-credentials toto --client-key=toto.key --client-certificate=toto.crt --embed-certs=true
 
-kubectl config set-context pbackz --cluster=default --user=pbackz
+kubectl config set-context toto --cluster=default --user=toto
 
-kubectl config use-context pbackz
+kubectl config use-context toto
 
 kubectl config current-context
 
